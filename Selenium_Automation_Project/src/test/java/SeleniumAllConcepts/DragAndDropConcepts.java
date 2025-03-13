3c5fdd2f6faa4853c047d970236fc96fee0f8f27
Ujjwal Tyagi <ujjwaltyagi9700@gmail.com>
2025-03-13 14:44:21 +0530

    DragAndDrop Concepts

---
+++ Selenium_Automation_Project/src/test/java/SeleniumAllConcepts/DragAndDropConcepts.java
@@ -0,0 +1,30 @@
+package SeleniumAllConcepts;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class DragAndDropConcepts {
+    public static void main(String[] args) {
+       
+        WebDriver driver = new ChromeDriver();
+        
+        // Navigate to the page
+        driver.navigate().to("https://demoqa.com/droppable/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+     
+        Actions action = new Actions(driver);
+
+        WebElement Drag = driver.findElement(By.id("draggable"));
+        WebElement Drop = driver.findElement(By.id("droppable"));
+
+        action.clickAndHold(Drag).moveToElement(Drop).release().build().perform();
+  
+        driver.quit();
+    }
+}
