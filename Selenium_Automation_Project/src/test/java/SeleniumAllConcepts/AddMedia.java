0f93ccea9c8c0fc66e67a37712263627bfff11ce
Ujjwal Tyagi <ujjwaltyagi9700@gmail.com>
2025-04-09 14:27:10 +0530

    ThirdCommit

--- Selenium_Automation_Project/src/test/java/SeleniumAllConcepts/AddMedia.java
+++ Selenium_Automation_Project/src/test/java/SeleniumAllConcepts/AddMedia.java
@@ -11,79 +11,90 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
 public class AddMedia {
 	WebDriver driver;
+
 	@BeforeClass
 	public void Setup() {
-		//driver= new ChromeDriver();
+		// driver= new ChromeDriver();
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("window-size=1024,1024");
-		 driver = new ChromeDriver(options); 
+		driver = new ChromeDriver(options);
 		driver.navigate().to("https://dev.myschoolone.com/newworldschool");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.findElement(By.name("username")).sendKeys("admin@mso786");
 		driver.findElement(By.name("password")).sendKeys("38024008ec29");
 		driver.findElement(By.id("imrobot")).click();
-		WebElement button=driver.findElement(By.xpath("//a[contains(text(),'Sign')]"));
-		button.click();}
-	@Test(priority=1)
+		WebElement button = driver.findElement(By.xpath("//a[contains(text(),'Sign')]"));
+		button.click();
+	}
+
+	@Test(priority = 1)
 	public void NavigatetoAddLibraryPage() throws InterruptedException {
-	Thread.sleep(2000);
+		Thread.sleep(2000);
 		driver.navigate().to("https://dev.myschoolone.com/Web/Library/library_addmedia.php");
-		String URLofThePage=driver.getCurrentUrl();
-		System.out.println("URL of The Add Library Page is : " + URLofThePage);
+		String URLofThePage = driver.getCurrentUrl();
+		Reporter.log("URL of The Add Library Page is : " + URLofThePage, true);
 		Assert.assertEquals(URLofThePage, "https://dev.myschoolone.com/Web/Library/library_addmedia.php");
 	}
-	@Test(priority=2)
+
+	@Test(priority = 2)
 	public void TitleofThePage() {
-		String TitleofThePage= driver.getTitle();
-		System.out.println("Title of The Page is : " + TitleofThePage);
-		Assert.assertEquals(TitleofThePage,"MySchoolOne");
+		String TitleofThePage = driver.getTitle();
+		Reporter.log("Title of The Page is : " + TitleofThePage, true);
+		Assert.assertEquals(TitleofThePage, "MySchoolOne");
 	}
+
 	@Test(priority = 3)
 	public void titleOfSubmodule() {
-	    WebElement titleOfSubmodule = driver.findElement(By.xpath("//h4[text()='Add Media']"));
-	    String actualTitle = titleOfSubmodule.getText();
-	    System.out.println("Title of Submodule is: " + actualTitle);
-	    Assert.assertEquals(actualTitle, "Add Media", "Submodule title does not match!");
+		WebElement titleOfSubmodule = driver.findElement(By.xpath("//h4[text()='Add Media']"));
+		String actualTitle = titleOfSubmodule.getText();
+		Reporter.log("Title of Submodule is: " + actualTitle, true);
+		Assert.assertEquals(actualTitle, "Add Media", "Submodule title does not match!");
 	}
-@Test(priority=4)
-public void SelectTheLibrary() throws InterruptedException {
-	WebElement Library= driver.findElement(By.name("library"));
-	Select lib= new Select(Library);
-	lib.selectByVisibleText("Middle School");
-	}
-@Test(priority=5)
-public void Registration() {
-    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-    wait.until(ExpectedConditions.presenceOfElementLocated(By.id("type_id")));
-    wait.until(ExpectedConditions.elementToBeClickable(By.id("type_id")));
-    WebElement registrationDropdown = driver.findElement(By.id("type_id"));
-    Select reg = new Select(registrationDropdown);
-    reg.selectByVisibleText("library 1");
-}
-@Test(priority=6)
-public void NoCopies() {
-	WebElement NoCopies= driver.findElement(By.name("copies"));
-	boolean Copies= NoCopies.isEnabled();
-	Assert.assertTrue(Copies, "No of Copies Text Field is not Enabled");
-    JavascriptExecutor js = (JavascriptExecutor) driver;
-    js.executeScript("arguments[0].value='3';", NoCopies);
-}
-@Test(priority=7)
-public void GetAttribute() {
-	WebElement GetAttributeCopies= driver.findElement(By.name("copies"));
-	System.out.println("Value of Attribure is : " + GetAttributeCopies.getAttribute("type") );
-	JavascriptExecutor js = (JavascriptExecutor) driver;
-	js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
 
-}
-@AfterClass
-public void TearDown() {
-	driver.quit();
-}
+	@Test(priority = 4)
+	public void SelectTheLibrary() throws InterruptedException {
+		WebElement Library = driver.findElement(By.name("library"));
+		Select lib = new Select(Library);
+		lib.selectByVisibleText("Middle School");
 	}
+
+	@Test(priority = 5)
+	public void Registration() {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("type_id")));
+		wait.until(ExpectedConditions.elementToBeClickable(By.id("type_id")));
+		WebElement registrationDropdown = driver.findElement(By.id("type_id"));
+		Select reg = new Select(registrationDropdown);
+		reg.selectByVisibleText("library 1");
+	}
+
+	@Test(priority = 6)
+	public void NoCopies() {
+		WebElement NoCopies = driver.findElement(By.name("copies"));
+		boolean Copies = NoCopies.isEnabled();
+		Assert.assertTrue(Copies, "No of Copies Text Field is not Enabled");
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].value='3';", NoCopies);
+	}
+
+	@Test(priority = 7)
+	public void GetAttribute() {
+		WebElement GetAttributeCopies = driver.findElement(By.name("copies"));
+		Reporter.log("Value of Attribure is : " + GetAttributeCopies.getAttribute("type"), true);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+	}
+
+	@AfterClass
+	public void TearDown() {
+		driver.quit();
+	}
+}
---
+++ Selenium_Automation_Project/src/test/java/SeleniumAllConcepts/AutomationExercise.java
@@ -0,0 +1,100 @@
+package SeleniumAllConcepts;
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class AutomationExercise {
+	WebDriver driver;
+	@BeforeClass
+	public void Setup() {
+		driver = new ChromeDriver();	
+		driver.get("https://practicetestautomation.com/practice-test-login/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));	
+	}
+	@Test(priority=1)
+	public void VerifyTheUserNameFunctionality() {
+		WebElement UserName = driver.findElement(By.id("username"));
+		boolean username = UserName.isEnabled();
+		Assert.assertTrue(username, "UserName Text Field is Not Enabled");
+		UserName.sendKeys("student");
+	}
+	@Test(priority=2,dependsOnMethods = "VerifyTheUserNameFunctionality")
+	public void VerifyTheUserNameLabel() {
+		WebElement UserNameLabel = driver.findElement(By.xpath("//label[text()=\"Username\"]"));
+		String LabelName = UserNameLabel.getText();
+		Reporter.log("Label Name is : " + LabelName, true);
+		Assert.assertEquals(LabelName, "Username", "Label is not As per Expected Result");
+	}   
+	@Test(priority=3,dependsOnMethods = "VerifyTheUserNameLabel")
+	public void VerifyTheTitleOfThePage() {
+		String TitleofThePage = driver.getTitle();
+		Reporter.log("Title of The Page is : " + TitleofThePage, true);
+		Assert.assertEquals(TitleofThePage, "Test Login | Practice Test Automation");
+	}
+	@Test(priority=4,dependsOnMethods = "VerifyTheTitleOfThePage")
+	public void VerifyTheURLofThePage() {
+		String URLofThePage = driver.getCurrentUrl();
+		Reporter.log("URL of The Page is : " + URLofThePage, true);
+		Assert.assertEquals(URLofThePage, "https://practicetestautomation.com/practice-test-login/");
+	}
+	@Test(priority=5,dependsOnMethods = "VerifyTheURLofThePage")
+	public void VerifyThePasswordFunctionality() {
+		WebElement Password = driver.findElement(By.id("password"));
+		boolean password = Password.isEnabled();
+		Assert.assertTrue(password, "Password Text Field is not Enabled");
+		Password.sendKeys("Password123");
+	}
+	@Test(priority=6,dependsOnMethods = "VerifyThePasswordFunctionality")
+	public void VerifyTheSubmitButtonFunctionality() {
+		WebElement Submit = driver.findElement(By.id("submit"));
+		boolean submit = Submit.isDisplayed();
+		Assert.assertTrue(submit, "Submit Button is not Display");
+		boolean Button = Submit.isEnabled();
+		Assert.assertTrue(Button, "Submit Button is not Enabled");
+		Submit.click();
+	}
+	@Test(priority=7,dependsOnMethods = "VerifyTheSubmitButtonFunctionality")
+	public void VerifyLoginSuccessMessage() {
+		WebElement Message = driver.findElement(By.xpath("//strong[text()=\"Congratulations student. You successfully logged in!\"]"));
+		String SuccessMessage = Message.getText();
+		Reporter.log("Success Message is : " + SuccessMessage, true);
+		boolean message = Message.isDisplayed();
+		Assert.assertTrue(message, "Success Message is not Display");
+	}
+	@Test(priority=8,dependsOnMethods = "VerifyLoginSuccessMessage")
+	public void VerifyTheLogOutButtonFunctionality() {
+		WebElement LogOut = driver.findElement(By.xpath("//a[normalize-space()='Log out']"));
+		boolean logout = LogOut.isDisplayed();
+		Assert.assertTrue(logout, "LogOut Button is Not Display");
+	}
+	@Test(priority=0)
+	public void VerifytheApplicationLogo() {
+		WebElement Logo= driver.findElement(By.xpath("//img[@alt='Practice Test Automation']"));
+		boolean logo= Logo.isDisplayed();
+		Assert.assertTrue(logo, "Application Logo is not Dispaly");
+	}
+	@Test(priority=9)
+	public void NumberofLink() {
+		List<WebElement> Links = driver.findElements(By.tagName("a"));
+		int NumberofLink = Links.size();
+		Reporter.log("Number of Link in This Page is : " + NumberofLink );
+		for(WebElement link : Links) {
+			Reporter.log("Name of The Links is : " + link.getText());
+		}
+		  Assert.assertEquals(NumberofLink, 10, "Unexpected number of links on the page.");
+	}
+	@AfterClass
+	public void TearDown() {
+		driver.quit();
+	}
+}
